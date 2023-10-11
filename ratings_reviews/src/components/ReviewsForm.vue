86e95e538a0aff8aa0aba72f7b6f20af61fcfab7
SayedMudassir Shahid <sayedmudassir.10725380@ltimindtree.com>
2023-10-11 21:37:06 +0530

    extra changes

--- ratings_reviews/src/components/ReviewsForm.vue
+++ ratings_reviews/src/components/ReviewsForm.vue
@@ -1,5 +1,6 @@
 <template>
-  <Card :reverse="true">
+  <Card>
+    <!-- <Card :reverse="true"> -->
     <form @submit.prevent="handleSubmit">
       <h2>How would you rate your service with us?</h2>
       <!-- Rating Component -->
